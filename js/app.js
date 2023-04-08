c429fcedc76b59c9c80f49106b32c2d1a3c83fee
Mustapha Alahji Ibrahim <iamustapha213@gmail.com>
2023-04-09 00:36:31 +0100

    added background effect

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+let body = document.querySelector("body");
+
 let range = document.querySelector("button");
 
 let rangeDisplay = document.querySelector("h5");
@@ -15,9 +17,12 @@ function rangeFunction(){
   let guess = Number(prompt("What is the number you choose"));
 
   if(guess == shuffle){
+    // body.style.backgroundColor = 'green';
     guessDisplay.textContent = `You Won this round : ${guess}`;
   }else{
+    // body.style.backgroundColor = 'red';
     guessDisplay.textContent = `Try again next time the number is ${shuffle}`;
   }
   
 }
+
