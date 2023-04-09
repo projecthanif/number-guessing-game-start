47e61513b601785223867ea27000ce03a6d4d80b
Mustapha Alahji Ibrahim <iamustapha213@gmail.com>
2023-04-09 01:12:42 +0100

    NaN

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ let guessDisplay = document.getElementById("guess");
 range.addEventListener("click",rangeFunction);
 
 function rangeFunction(){
-  let getNumber = prompt("What is the range Number you choose");
+  let getNumber = Number(prompt("What is the range Number you choose"));
   rangeDisplay.textContent = `The range you choose is ${getNumber}`;
 
   let shuffle = Math.floor(Math.random() * getNumber) + 1;
